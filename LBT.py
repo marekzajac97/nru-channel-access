862c7edc4bf93e14e1f28a486600a503d2efe6c5
Marek Zajac <marekzajac97@gmail.com>
2020-09-30 19:12:54 +0200

    sync slots and reservation signals initial version

--- LBT.py
+++ LBT.py
@@ -3,37 +3,44 @@ import simpy
 
 
 RANDOM_SEED = 42
-TRANSMITION_TIME = 8					# Mean tranmission time in miliseconds
-SIM_TIME = 1							# Simulation time in seconds
+SIM_TIME = 0.1							# Simulation time in seconds
 GNB_NUMBER = 2							# Number of gNBs/nodes
 
 DETER_PERIOD = 16						# Time which a node is required to wait at the start of prioritization period (16 us)
 OBSERVATION_SLOT_DURATION = 9			# observation slot length in microseconds
+SYNCHRONIZATION_SLOT_DURATION = 1000	# synchronization slot length in microseconds
 
 # Channel access class 1
 # M = 1 					 # fixed number of observation slots in prioritization period
 # CW_MIN = 3				 # minimum contention window size
 # CW_MAX = 7			     # maximum
+# MCOT = 2
 
 # Channel access class 2
 # M = 1
 # CW_MIN = 7
 # CW_MAX = 15
+# MCOT = 3
 
 # Channel access class 3
 # M = 2
 # CW_MIN = 15
 # CW_MAX = 63
+# MCOT = 8
 
 # Channel access class 4
 M = 7
 CW_MIN = 15
 CW_MAX = 1023
+MCOT = 8
 
 
 def log(output):
 	pass
-	# print(output)
+	# if 'gNB 0' in output:
+	# 	print("\033[35m" + output + "\033[0m")
+	# else:
+	# 	print(output)
 
 def log_fail(output):
 	pass
@@ -87,12 +94,20 @@ class Gnb(object):
 		self.env = env
 		self.channel = channel
 		self.id = id
-		self.cw = CW_MIN			# current contention window size
-		self.N = None				# backoff counter
-		self.successful_trans = 0	# number of successful transmissions
-		self.total_trans = 0		# total number of transmissions
+		self.cw = CW_MIN				# current contention window size
+		self.N = None					# backoff counter
+		self.next_sync_slot_boundry = 0	# nex synchronization slot boundry
+		self.successful_trans = 0		# number of successful transmissions
+		self.total_trans = 0			# total number of transmissions
 
 		self.env.process(self.run())
+
+	def sync_slot_counter(self):
+		"""Process responsible for keeping the next sync slot boundry timestamp"""
+		while True:
+			self.next_sync_slot_boundry += SYNCHRONIZATION_SLOT_DURATION
+			# log_fail("{:.2f}:\t {} SYNC SLOT BOUNDRY NOW".format(self.env.now, self.id))
+			yield self.env.timeout(SYNCHRONIZATION_SLOT_DURATION)
 
 	def transmit(self, transmission):
 		"""Start occupying the channel for the transmission's duration and check for collison"""
@@ -145,9 +160,9 @@ class Gnb(object):
 		self.N = yield sensing_proc
 		self.channel.sensing_processes.remove(sensing_proc)
 
-
 	def run(self):
 		"""Main process. Genrate new transmission, wait for channel to become idle and begin transmission"""
+		self.env.process(self.sync_slot_counter())
 		while True:
 			log("{:.2f}:\t {} begins new transmisson procedure".format(self.env.now, self.id))
 			
@@ -160,7 +175,13 @@ class Gnb(object):
 				if self.N != 0:
 					log("{:.2f}:\t {} backoff is frozen".format(self.env.now, self.id))
 
-			trans_time = random.expovariate(1.0 / TRANSMITION_TIME) * 10e3 # TODO take MCOT into account
+			# reserve channel while waiting for the next sync slot boundry
+			time_to_next_sync_slot = self.next_sync_slot_boundry - self.env.now
+			log("{:.2f}:\t {} transmiting reservation signal while waiting for a new slot boundry for the next {:.2f}".format(self.env.now, self.id, time_to_next_sync_slot))
+			reservation_signal = Transmission(self.env.now, self.env.now + time_to_next_sync_slot, time_to_next_sync_slot)
+			yield self.env.process(self.channel.occupy(reservation_signal))
+
+			trans_time = (MCOT * 1e3 - time_to_next_sync_slot) # substract time taken for transmiting reservation signal to fulfill MCOT constraint
 			transmission = Transmission(self.env.now, self.env.now + trans_time, trans_time)
 			yield self.env.process(self.transmit(transmission))
 			if not transmission.collided:
@@ -181,7 +202,7 @@ env = simpy.Environment()
 channel = Channel(env)
 gnbs = [Gnb(env, 'gNB {}'.format(i), channel) for i in range(GNB_NUMBER)]
 
-env.run(until=(SIM_TIME*10e6))
+env.run(until=(SIM_TIME*1e6))
 
 total_transmissions = 0
 successful_transmissions = 0
